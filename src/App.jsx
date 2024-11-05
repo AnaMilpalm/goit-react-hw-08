e8a13af3f1e4846fc857d5e981181f6dcc09f8bb
Anastacy Slyusar <ana.milpalm@gmail.com>
2024-11-05 11:52:57 +0100

    select and checkbox were added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@
 
 import './App.css'
 import ContactForm from './components/ContactForm/ContactForm'
-import LoginForm from './components/ContactForm/LoginForm/LoginForm'
+import LoginForm, { LoginFormThird, MyComponent } from './components/ContactForm/LoginForm/LoginForm'
 import LoginFormSecond from './components/LoginFormSecond/LoginFormSecond'
 import { useState } from 'react';
+import SearchBar from './components/SearchBar/SearchBar';
+import LangSwitcher, { CheckBox, RadioButton } from './components/Select/Select';
 
 
 function App() {
@@ -21,8 +23,14 @@ function App() {
     <ContactForm />
     <LoginForm />
     <LoginFormSecond onLogin={handleLogin}/>
+    <MyComponent />
+    <LoginFormThird />
+    <SearchBar />
+    <LangSwitcher />
+    <RadioButton />
+    <CheckBox />
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/ContactForm/LoginForm/LoginForm.jsx
+++ src/components/ContactForm/LoginForm/LoginForm.jsx
@@ -1,3 +1,5 @@
+import { useId } from 'react';
+
 const LoginForm = () => {
 
     const handleSubmit = (evt) => {
@@ -7,7 +9,7 @@ const LoginForm = () => {
         console.log(form);
         const { login, password } = form.elements;
 
-        
+
         console.log(login, password);// посилання на дом елементи
 
         console.log(login.value, password.value)// значення полів
@@ -25,4 +27,33 @@ const LoginForm = () => {
      )
 }
 
+export const MyComponent = () => {
+     const id = useId();
+
+     return (
+        <div>
+            <label htmlFor={id}>Text field label</label>
+            <input type="text" id={id} />
+        </div>
+     );
+};
+
 export default LoginForm;
+
+export const LoginFormThird = () => {
+    const loginId = useId();
+    const passwordId = useId();
+  
+    return (
+      <form>
+        <label htmlFor={loginId}>Login</label>
+        <input type="text" name="login" id={loginId} />
+  
+        <label htmlFor={passwordId}>Password</label>
+        <input type="password" name="password" id={passwordId} />
+  
+        <button type="submit">Login</button>
+      </form>
+    );
+  };
+  
