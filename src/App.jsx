8ea723dd97ad60aa8022ce75d47e028618af7731
Anastacy Slyusar <ana.milpalm@gmail.com>
2024-12-12 23:08:05 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useSelector } from "react-redux";
-import { selectIsLoading, selectIsError } from "./redux/contactsSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchAllContacts } from "./redux/contactsOps";
 import ContactForm from "./components/ContactForm/ContactForm";
-import SearchBox from "./components/SearchBox/SearchBox";
 import ContactsList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 import "./App.css";
 import "modern-normalize";
 
 function App() {
-  const isLoading = useSelector(selectIsLoading);
-  const isError = useSelector(selectIsError);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllContacts());
+  }, [dispatch]);
 
   return (
     <div className="wrapper">
       <h1>Phonebook</h1>
-      {isError && <p className="error">Error: {isError}</p>}
-      {isLoading && <p className="loading">Loading...</p>}
       <ContactForm />
       <SearchBox />
       <ContactsList />
