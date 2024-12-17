a643c3cb05ba0c5b8b104b56b6447cd1dacd8607
Anastacy Slyusar <ana.milpalm@gmail.com>
2024-12-17 19:06:51 +0100

    fetch 401

--- src/redux/contacts/contactsOps.js
+++ src/redux/contacts/contactsOps.js
@@ -1,13 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
 
-axios.defaults.baseURL = "https://connections-api.goit.global/";
+import { goitApi } from "../auth/operations";
+
+// axios.defaults.baseURL = "https://connections-api.goit.global/";
+
+// axios.defaults.baseURL = goitApi;
 
 export const fetchAllContacts = createAsyncThunk(
   "contacts/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/contacts");
+      const response = await goitApi.get("/contacts");
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -19,7 +22,7 @@ export const addContact = createAsyncThunk(
   "contacts/addContact",
   async (contact, thunkAPI) => {
     try {
-      const response = await axios.post("/contacts", contact);
+      const response = await goitApi.post("/contacts", contact);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -31,7 +34,7 @@ export const deleteContact = createAsyncThunk(
   "contacts/deleteContact",
   async (id, thunkAPI) => {
     try {
-      const response = await axios.delete(`/contacts/${id}`);
+      const response = await goitApi.delete(`/contacts/${id}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
